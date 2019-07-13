abb6be7957fd088ba72a754f29ab68780fbe42d1
Jan <j.kozub136@gmail.com>
2019-07-13 08:43:23 +0200

    first commit

---
+++ ClockMQTT.ino
@@ -0,0 +1,91 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+#define relay 19
+#define dots 27
+
+#define latchPin1 4
+#define dataPin1 16
+#define clockPin1 2
+
+#define latchPin2 5
+#define dataPin2 18
+#define clockPin2 17
+
+#define latchPin3 12
+#define dataPin3 14
+#define clockPin3 13
+
+#define latchPin4 21
+#define dataPin4 23
+#define clockPin4 22
+
+byte dec_digits[] = {0b11000000, 0b11111001, 0b10100100, 0b10110000, 0b10011001, 0b10010010, 0b10000011, 0b11111000, 0b10000000, 0b10011000 };
+hw_timer_t *timer = NULL;
+
+const char* ssid     = "KOZUB n-kom.pl";
+const char* password = "81427840";
+const char* mqtt_server = "10.0.98.125";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+String formattedDate;
+String timeStamp;
+
+int lastTime1 = 0;
+int lastTime2 = 0;
+int lastTime3 = 0;
+
+boolean dotsState = true;
+boolean state = true;
+
+void setup() {
+  pinMode(dots, OUTPUT);
+  pinMode(relay, OUTPUT);
+  digitalWrite(relay, state);
+
+  pinMode(latchPin1, OUTPUT);
+  pinMode(clockPin1, OUTPUT);
+  pinMode(dataPin1, OUTPUT);
+  pinMode(latchPin2, OUTPUT);
+  pinMode(clockPin2, OUTPUT);
+  pinMode(dataPin2, OUTPUT);
+  pinMode(latchPin3, OUTPUT);
+  pinMode(clockPin3, OUTPUT);
+  pinMode(dataPin3, OUTPUT);
+  pinMode(latchPin4, OUTPUT);
+  pinMode(clockPin4, OUTPUT);
+  pinMode(dataPin4, OUTPUT);
+
+  Serial.begin(115200);
+
+  setup_wifi();
+
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+void loop() {
+  long now = millis();
+
+  if (now - lastTime2 > 1000) {
+    lastTime2 = now;
+    dotsState = !dotsState;
+    digitalWrite(dots, dotsState);
+  }
+
+  if (!client.connected()) {
+    reconnect();
+  } else {
+    client.loop();
+
+    if (now - lastTime3 > 2000) {
+      lastTime3 = now;
+      String msg = "clock" + String(state);
+      Serial.print("Publish message: ");
+      Serial.println(msg);
+      client.publish("clock", msg.c_str());
+     
+    }
+  }
+}
---
+++ MqttService.ino
@@ -0,0 +1,70 @@
+void setup_wifi() {
+
+  delay(10);
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("WiFi connected");
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    String clientId = "Clock";
+
+    clientId += String(random(0xffff), HEX);
+    if (client.connect(clientId.c_str())) {
+      Serial.println("connected");
+
+      client.publish("clock", "clock");
+
+      client.subscribe("clockSub");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+
+      delay(2000);
+    }
+  }
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  String output = "";
+  for (int i = 0; i < length; i++) {
+    output = output + (char)payload[i];
+  }
+  if (output.equals("TOGGLE")) {
+    state = !state;
+    String msg = "clock" + String(state);
+    client.publish("clock", msg.c_str());
+    digitalWrite(relay, state);
+  } else {
+    char timeArray[8];
+    
+    output.toCharArray(timeArray, 8);
+
+    digitalWrite(latchPin1, LOW);
+    digitalWrite(latchPin2, LOW);
+    digitalWrite(latchPin3, LOW);
+    digitalWrite(latchPin4, LOW);
+
+    shiftOut(dataPin1, clockPin1, MSBFIRST, dec_digits[(int)timeArray[0] - 48]);
+    shiftOut(dataPin2, clockPin2, MSBFIRST, dec_digits[(int)timeArray[1] - 48]);
+    shiftOut(dataPin3, clockPin3, MSBFIRST, dec_digits[(int)timeArray[2] - 48]);
+    shiftOut(dataPin4, clockPin4, MSBFIRST, dec_digits[(int)timeArray[3] - 48]);
+
+    digitalWrite(latchPin1, HIGH);
+    digitalWrite(latchPin2, HIGH);
+    digitalWrite(latchPin3, HIGH);
+    digitalWrite(latchPin4, HIGH);
+  }
+}
